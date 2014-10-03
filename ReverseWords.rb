13138d96be8c9f3aaddccf8eccc70bf95eac27b0
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-10-02 23:58:57 -0500

    Reversed Words

---
+++ ReverseWords.rb
@@ -0,0 +1,14 @@
+File.open(ARGV[0]).each_line do |line|
+
+	line.strip!
+	if line != ''
+		arr = line.split
+		while arr.length > 0
+			print arr.pop
+			print " " if arr.length > 0
+		end
+		print "\n"
+	end
+
+
+end
