43e3df7d37015c4885b85fb49b0d034e7c0ccf0d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-10-12 00:14:12 -0500

    Longest words

--- Moderate/LongestLines.rb
+++ Moderate/LongestLines.rb
@@ -1,13 +1,38 @@
 arg = File.readlines(ARGV[0])
-print arg
-exit
-.each_line do |line|
+n = arg.first.to_i
+arg.delete_at 0
 
-  line.strip!
+strings = []
+longest = [nil,0]
+arg.each_with_index do |line,index|
 
-  if line != ''
+    line.strip!
 
-  end
-
-
+    if line != ''
+      l = line.length
+      if (l > longest[1])
+        longest[0] = index
+        longest[1] = l
+      end
+      strings.push(l)
+    end
 end
+
+puts arg[longest[0]]
+arg.delete_at(longest[0])
+strings.delete_at(longest[0])
+
+(1..n-1).each do |n|
+
+  longest = [nil,0]
+  strings.each_with_index do |s,index|
+    if (s > longest[1])
+      longest[0] = index
+      longest[1] = s
+    end
+  end
+  puts arg[longest[0]]
+  arg.delete_at(longest[0])
+  strings.delete_at(longest[0])
+end
+
