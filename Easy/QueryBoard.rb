2f110004d007fbfbbf716ae7508da4eedd7bf95e
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-10-16 00:12:47 -0500

    Query Board

--- Easy/QueryBoard.rb
+++ Easy/QueryBoard.rb
@@ -23,13 +23,27 @@ class Board
     @board.each do |row|
       res += row[n]
     end
+    puts res
+  end
+  def QueryRow(n)
+    res = 0
+    @board[n].each do |val|
+      res +=val
+    end
+    puts res
   end
 
 end
-
-b = Board.new
-b.SetRow(255, 1)
-puts b.QueryCol 0
-
-# //values = Array.new()
-
+board = Board.new
+File.open(ARGV[0]).each_line do |line|
+  line.strip!
+  if line != ''
+    v = nil
+    command, n, v = line.split(' ')
+    if !v.nil?
+      board.send(command,n.to_i,v.to_i)
+    else
+      board.send(command,n.to_i)
+    end
+  end
+end
