ee68d81c46042c23f8c0d745c279ac8e0e8cb02b
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-10-21 00:11:48 -0500

    JSON Menu IDs

--- Easy/JSONMenuIDS.rb
+++ Easy/JSONMenuIDS.rb
@@ -1,1 +1,1 @@
-puts JSON.parse('{"menu": {"header": "menu", "items": [{"id": 27}, {"id": 0, "label": "Label 0"}, null, {"id": 93}, {"id": 85}, {"id": 54}, null, {"id": 46, "label": "Label 46"}]}}')
+git adgita
