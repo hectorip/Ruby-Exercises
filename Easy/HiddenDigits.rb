f515c079045350143dd3ec81737e274e2e94f424
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-10-29 00:24:08 -0600

    Hidden Digits

--- Easy/HiddenDigits.rb
+++ Easy/HiddenDigits.rb
@@ -1,16 +1,24 @@
-i = 0
-valid_letters = Hash[('a'..'j').map {|l| [l,i]}.invert
-print valid_letters
-# File.open(ARGV[0]).each_line do |line|
-#   line.strip!
-#   if line != ''
-#     result = ''
-#     line.each_char do |c|
-#       unless valid_letters[c].nil?
-#
-#       end
-#     end
-#
-#   end
-#
-# end
+letters = ('a'..'j').to_a
+values = ('0'..'9').to_a
+valid = Hash.new
+letters.each_with_index do |l,index|
+  valid[l] = values[index]
+  valid[values[index]]= values[index]
+end
+File.open(ARGV[0]).each_line do |line|
+  line.strip!
+  if line != ''
+    found = false
+    line.each_char do |c|
+      if !valid[c].nil?
+        found = true
+        print valid[c]
+      end
+    end
+    unless found
+      print 'NONE'
+    end
+    print "\n"
+  end
+
+end
