627dbc6936e8f0544cb75a64c346e7c2f3750b01
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2014-12-01 00:19:58 -0600

    Data Recovery

--- Easy/DataRecovery.rb
+++ Easy/DataRecovery.rb
@@ -1,3 +1,18 @@
 File.open(ARGV[0]).each_line do |line|
+  line.strip!
+  if line != ''
 
+    words,numbers = line.split(';')
+    words = words.split(' ').map { |word| word.strip}
+    numbers = numbers.split(' ').map { |n| n.to_i}
+    ordered = []
+    sequence = (1..words.length).to_a
+    last_word = words.pop
+    words.each_with_index do |word, index|
+      ordered[numbers[index]] = word
+      sequence.delete(numbers[index])
+    end
+    ordered[sequence[0]] = last_word
+    puts ordered.join(' ')
+  end
 end
